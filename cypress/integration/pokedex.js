9fdd29fd17f7b4a484ee9919418387b117775ca4
Gabriel Aguilar <gabagurod@outlook.es>
2021-11-12 15:59:27 +0100

    Added new Pokemon page e2e test

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -5,4 +5,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Pokemon page can be accessed from main page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('squirtle').click()
+    cy.contains('rain dish')
+  })
 })
